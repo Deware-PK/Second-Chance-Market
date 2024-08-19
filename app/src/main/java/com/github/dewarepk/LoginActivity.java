c6c34c6a7869dffc046600c7c7e5068316354608
Deware-PK <phakin.kus@gmail.com>
2024-08-20 00:35:16 +0700

    Make login button working.

--- app/src/main/java/com/github/dewarepk/LoginActivity.java
+++ app/src/main/java/com/github/dewarepk/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.github.dewarepk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,9 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(view -> {
             if(username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                 Toast.makeText(LoginActivity.this, "Login Successful",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, StoreActivity.class);
+                this.startActivity(intent);
+                this.finish();
             }else {
                 Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
             }
