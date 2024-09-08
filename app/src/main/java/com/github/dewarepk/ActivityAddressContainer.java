f3f12cf4853b853678d2eb33602117d35d8122a7
Deware-PK <phakin.kus@gmail.com>
2024-09-08 19:44:49 +0700

    New UI

---
+++ app/src/main/java/com/github/dewarepk/ActivityAddressContainer.java
@@ -0,0 +1,21 @@
+package com.github.dewarepk;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityAddressContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_address_container);
+
+
+    }
+}
