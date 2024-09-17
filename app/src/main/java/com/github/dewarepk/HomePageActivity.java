267e93ea1545e4182762bd916265ca8eeb96ebc6
Deware-PK <phakin.kus@gmail.com>
2024-09-17 23:37:57 +0700

    Create RecycleView Adapter

--- app/src/main/java/com/github/dewarepk/HomePageActivity.java
+++ app/src/main/java/com/github/dewarepk/HomePageActivity.java
@@ -1,17 +1,25 @@
 package com.github.dewarepk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.github.dewarepk.model.SimpleCallback;
+import com.github.dewarepk.model.ItemData;
+import com.github.dewarepk.model.ItemType;
+import com.github.dewarepk.model.widget.ItemAdapter;
 import com.github.dewarepk.util.SimpleUtil;
 import com.github.dewarepk.util.ValidateUtil;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -23,9 +31,42 @@ public class HomePageActivity extends AppCompatActivity {
 
         ValidateUtil.checkIntegrity(this.getApplicationContext(), this);
 
+        ArrayList<ItemData> data = new ArrayList<>();
+        loadData(data);
+
         TextView fullNameDisplay = this.findViewById(R.id.fullname_display);
 
         SimpleUtil.getUserFullName(this.getApplicationContext(), fullNameDisplay::setText);
 
+        LinearLayout profileButton = this.findViewById(R.id.me_btn);
+
+        profileButton.setOnClickListener(aVoid -> this.startActivity(new Intent(HomePageActivity.this, TopUpActivity.class)));
+
+
+
+        ItemAdapter adapter = new ItemAdapter(this, data);
+        RecyclerView recyclerView = this.findViewById(R.id.recommendView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void loadData(List<ItemData> data) {
+        data.add(new ItemData("0" , "shirt_holder", 5, 4.5f, 50.0 , ItemType.CLOTHE));
+        data.add(new ItemData("1" , "https://play-lh.googleusercontent.com/QPo012nDmGHkDLe7MtXkP4J7y_W_MZ8VgfyY6Iqx79ePl_n-0o6cDFLKI0ATdMEqea2Gnq_WtQEY32U5Zg", 5, 4.5f, 50.0 , ItemType.CLOTHE));
+        data.add(new ItemData("2" , "box", 2, 4.5f, 150.0 , ItemType.CLOTHE));
+        data.add(new ItemData("3" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("4" , "box", 2, 4.5f, 150.0 , ItemType.CLOTHE));
+        data.add(new ItemData("5" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("6" , "box", 2, 4.5f, 150.0 , ItemType.CLOTHE));
+        data.add(new ItemData("7" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("8" , "box", 2, 4.5f, 150.0 , ItemType.CLOTHE));
+        data.add(new ItemData("9" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("10" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("11" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("12" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("13" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("14" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
+        data.add(new ItemData("15" , "user", 1, 4.5f, 500.0 , ItemType.CLOTHE));
     }
 }
---
+++ app/src/main/java/com/github/dewarepk/model/widget/ItemAdapter.java
@@ -0,0 +1,69 @@
+package com.github.dewarepk.model.widget;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.dewarepk.R;
+import com.github.dewarepk.model.ItemData;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+
+    private final ArrayList<ItemData> items;
+    private final Context context;
+
+    public ItemAdapter(Context context, ArrayList<ItemData> items) {
+        this.items = items;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.viewholder_item_list, parent, false);
+
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemAdapter.ItemViewHolder holder, int position) {
+        ItemData currentItem = items.get(position);
+
+        holder.titleTxt.setText(currentItem.getHeader());
+        holder.priceText.setText("Price: $" + currentItem.getPrice());
+
+        Picasso.get()
+                .load(currentItem.getPictureUrl())
+                .placeholder(R.drawable.shirt_holder)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView titleTxt, priceText;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            priceText = itemView.findViewById(R.id.price_holder);
+        }
+    }
+
+}
